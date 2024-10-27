c3fb3781920b7c700b7aede69353fbfe3f54d8e0
Dinobonecrash1 <128608588+Dinobonecrash1@users.noreply.github.com>
2024-10-27 12:25:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,8 +106,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "http://142.93.209.247:8082/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "http://142.93.209.247:8082/".format(FQDN, PORT)
+URL = "https://filetolink.mknbotz.workers.dev".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://filetolink.mknbotz.workers.dev".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -122,9 +122,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "http://142.93.209.247:8082/".format(FQDN)
+    URL = "https://filetolink.mknbotz.workers.dev".format(FQDN)
 else:
-    URL = "http://142.93.209.247:8082/".format(FQDN)
+    URL = "https://filetolink.mknbotz.workers.dev".format(FQDN)
 
 # add premium logs channel id
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1001586987735'))
